7d94b366dcaa51d5cddddbe20c3664983bdce5b9
Rohit Raveendran <rohit.rav33ndran@gmail.com>
2021-05-27 18:45:20 +0200

    day16 and day17

---
+++ day16/turtlecolours.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color('DeepPink3')
+timmy.forward(100)
+myscreen = Screen()
+
+print(myscreen.canvheight)
+myscreen.exitonclick()
