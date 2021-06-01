fa4ba55f1c1dd675be308bcb7e4d794da4d3cbc0
Rohit Raveendran <rohit.rav33ndran@gmail.com>
2021-06-01 22:37:47 +0200

    day24

---
+++ day24-2/main.py
@@ -0,0 +1,15 @@
+# file = open("new_file.txt")
+# contents = file.read()
+# print(contents)
+# file.close()
+
+#With doesn't require a manual close methods.
+# with open("new_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+
+# Write to the file
+with open("new_file.txt", mode="w") as file:
+    file.write("What's happening")
+    
