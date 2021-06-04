3ab5047bef1394aff3f6e3a7f69701ff1f985500
Rohit Raveendran <rohit.rav33ndran@gmail.com>
2021-06-04 22:59:36 +0200

    day27 - tkinter

---
+++ day27/main.py
@@ -0,0 +1,20 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+
+
+#Label
+my_label = tkinter.Label(text="I am a Label", font=("Arial",24,"bold"))
+my_label.pack()
+
+
+
+
+
+
+
+
+
+window.mainloop()
